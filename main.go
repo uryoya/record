89af8d241f0c2c971616dbb4dd9789751193dd51
URANO Ryoya <urano.works.mail@gmail.com>
2018-06-07 06:06:15 +0900

    JSONへの変換

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	"os/exec"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
 	"syscall"
-	"io"
-	"os"
 )
 
+type RusageJson struct {
+	Utime   int64 `json:"utime"`
+	Stime   int64 `json:"stime"`
+	Maxrss  int64 `json:"maxrss"`
+	Minflt  int64 `json:"minflt"`
+	Majflt  int64 `json:"majflt"`
+	Inblock int64 `json:"inblock"`
+	Oublock int64 `json:"oublock"`
+	Nvcsw   int64 `json:"nvcsw"`
+	Nivcsw  int64 `json:"nivcsw"`
+}
+
 type RecordJson struct {
-	Stdout string
-	Stderr string
-	Status int
-	Rusage syscall.Rusage
+	Stdout string             `json:"stdout"`
+	Stderr string             `json:"stderr"`
+	Status syscall.WaitStatus `json:"status"`
+	Rusage RusageJson         `json:"rusage"`
 }
 
 func main() {
@@ -22,11 +34,37 @@ func main() {
 	if err := cmd.Start(); err != nil {
 		panic(err)
 	}
-	io.Copy(os.Stdout, stdout)
-	io.Copy(os.Stderr, stderr)
-	if err := cmd.Wait(); err != nil {
+	recStdout, err := ioutil.ReadAll(stdout)
+	if err != nil {
 		panic(err)
 	}
+	recStderr, err := ioutil.ReadAll(stderr)
+	if err != nil {
+		panic(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			exitErr.Sys()
+		}
+	}
+	status := cmd.ProcessState.Sys().(syscall.WaitStatus)
 	rusage := cmd.ProcessState.SysUsage().(*syscall.Rusage)
-	fmt.Println(rusage.Maxrss)
+	record := RecordJson{
+		string(recStdout),
+		string(recStderr),
+		status,
+		RusageJson{
+			rusage.Utime.Usec,
+			rusage.Stime.Usec,
+			rusage.Maxrss,
+			rusage.Minflt,
+			rusage.Majflt,
+			rusage.Inblock,
+			rusage.Oublock,
+			rusage.Nvcsw,
+			rusage.Nivcsw,
+		},
+	}
+	recordJson, _ := json.Marshal(record)
+	fmt.Println(string(recordJson))
 }
