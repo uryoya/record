346d73993c2830448b01ef104e99a11e0f017d76
URANO Ryoya <urano.works.mail@gmail.com>
2018-06-07 06:38:55 +0900

    計測部分をrunCommand()に分離

--- main.go
+++ main.go
@@ -30,26 +30,24 @@ type RecordJson struct {
 	Rusage RusageJson         `json:"rusage"`
 }
 
-func main() {
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+func runCommand(name string, args ...string) (*RecordJson, error) {
+	cmd := exec.Command(name, args...)
 	stdout, _ := cmd.StdoutPipe()
 	stderr, _ := cmd.StderrPipe()
 	start := time.Now().UnixNano()
 	if err := cmd.Start(); err != nil {
-		panic(err)
+		return nil, err
 	}
 	recStdout, err := ioutil.ReadAll(stdout)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	recStderr, err := ioutil.ReadAll(stderr)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	if err := cmd.Wait(); err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			exitErr.Sys()
-		}
+		return nil, err
 	}
 	end := time.Now().UnixNano()
 	status := cmd.ProcessState.Sys().(syscall.WaitStatus)
@@ -70,6 +68,14 @@ func main() {
 			rusage.Nvcsw,
 			rusage.Nivcsw,
 		},
+	}
+	return &record, nil
+}
+
+func main() {
+	record, err := runCommand(os.Args[1], os.Args[2:]...)
+	if err != nil {
+		panic(err)
 	}
 	recordJson, _ := json.Marshal(record)
 	fmt.Println(string(recordJson))
