de3e251a051b9231028a3393715d61a0de5637d1
URANO Ryoya <urano.works.mail@gmail.com>
2018-06-07 05:39:39 +0900

    物理メモリ使用量の表示

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"os/exec"
+	"fmt"
+	"syscall"
+	"io"
+	"os"
+)
+
+type RecordJson struct {
+	Stdout string
+	Stderr string
+	Status int
+	Rusage syscall.Rusage
+}
 
 func main() {
-	fmt.Println("record")
+	cmd := exec.Command("ls", "-la")
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, stdout)
+	io.Copy(os.Stderr, stderr)
+	if err := cmd.Wait(); err != nil {
+		panic(err)
+	}
+	rusage := cmd.ProcessState.SysUsage().(*syscall.Rusage)
+	fmt.Println(rusage.Maxrss)
 }
